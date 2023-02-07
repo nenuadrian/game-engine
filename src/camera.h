469ec223687a63e5c177901af351f459f70d5fa3
Nenu Adrian <adrian.nenu@gmail.com>
2023-02-07 08:46:41 +0000

    Update camera.h

--- src/camera.h
+++ src/camera.h
@@ -3,8 +3,6 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
-#include <vector>
-
 // Defines several possible options for camera movement. Used as abstraction to
 // stay away from window-system specific input methods
 enum Camera_Movement { FORWARD, BACKWARD, LEFT, RIGHT };
@@ -62,50 +60,9 @@ public:
   glm::mat4 GetViewMatrix() {
     return glm::lookAt(Position, Position + Front, Up);
   }
-
-  // processes input received from any keyboard-like input system. Accepts input
-  // parameter in the form of camera defined ENUM (to abstract it from windowing
-  // systems)
-  void ProcessKeyboard(Camera_Movement direction, float deltaTime) {
-    float velocity = MovementSpeed * deltaTime;
-    if (direction == FORWARD)
-      Position += Front * velocity;
-    if (direction == BACKWARD)
-      Position -= Front * velocity;
-    if (direction == LEFT)
-      Position -= Right * velocity;
-    if (direction == RIGHT)
-      Position += Right * velocity;
-  }
-
-  // processes input received from a mouse input system. Expects the offset
-  // value in both the x and y direction.
-  void ProcessMouseMovement(float xoffset, float yoffset) {
-    xoffset *= MouseSensitivity;
-    yoffset *= MouseSensitivity;
-
-    Yaw += xoffset;
-    Pitch += yoffset;
-
-    // make sure that when pitch is out of bounds, screen doesn't get flipped
-    if (Pitch > 89.0f)
-      Pitch = 89.0f;
-    if (Pitch < -89.0f)
-      Pitch = -89.0f;
-
-    // update Front, Right and Up Vectors using the updated Euler angles
-    updateCameraVectors();
-  }
-
-  // processes input received from a mouse scroll-wheel event. Only requires
-  // input on the vertical wheel-axis
-  void ProcessMouseScroll(float yoffset) {
-    Zoom -= (float)yoffset;
-    if (Zoom < 1.0f)
-      Zoom = 1.0f;
-    if (Zoom > 45.0f)
-      Zoom = 45.0f;
-  }
+  void ProcessKeyboard(Camera_Movement direction, float deltaTime);
+  void ProcessMouseMovement(float xoffset, float yoffset);
+  void ProcessMouseScroll(float yoffset);
 
 private:
   // calculates the front vector from the Camera's (updated) Euler Angles
