f1719c43649d258b1b92ad669e53f12af265bd51
Nenu Adrian <adrian.nenu@gmail.com>
2023-02-09 10:31:18 +0000

    cleanup

--- src/core/shader.h
+++ src/core/shader.h
@@ -2,9 +2,37 @@
 
 #pragma once
 
-#include "opengl.h"
-#include <string>
 #include <glm/glm.hpp>
+#include <string>
+
+#define GL_SILENCE_DEPRECATION
+
+#include <GLFW/glfw3.h>
+
+#include <OpenGL/gl3.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#define OPENGL_UTILS
+
+static void CheckOpenGLError(const char *stmt, const char *fname, int line) {
+  GLenum err = glGetError();
+  if (err != GL_NO_ERROR) {
+    // const GLubyte* msg = glErrorString(err);
+    printf("OpenGL error %08x, at %s:%i - for %s \n", err, fname, line, stmt);
+    abort();
+  }
+}
+
+#ifdef _DEBUG
+#define GL_CHECK(stmt)                                                         \
+  do {                                                                         \
+    stmt;                                                                      \
+    CheckOpenGLError(#stmt, __FILE__, __LINE__);                               \
+  } while (0)
+#else
+#define GL_CHECK(stmt) stmt
+#endif
 
 class Shader {
 public:
