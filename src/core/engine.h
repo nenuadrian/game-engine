bd9a0230a93b44266d689655f9e87d58871fa300
Nenu Adrian <adrian.nenu@gmail.com>
2023-02-14 08:40:24 +0000

    Update engine.h

--- src/core/engine.h
+++ src/core/engine.h
@@ -2,11 +2,20 @@
 #pragma once
 #include "events.h"
 #include <chrono>
+#include <vector>
+#include <string>
 
 namespace Hades {
-
+class Log {
+ public:
+  std::string msg;
+  
+  Log(std::string msg) : msg(msg) {};
+}
+  
 class Engine {
   Events events;
+  std::vector<Log> logs;
 
 public:
   Engine(){};
@@ -17,5 +26,13 @@ public:
         std::chrono::system_clock::now().time_since_epoch());
     return ms.count();
   };
+  
+  void info(std::string msg) {
+    logs.push_back(Log(msg));
+  }
+  
+  void error(std::string msg) {
+    logs.push_back(Log(msg));
+  }
 }; 
 } // namespace Hades
