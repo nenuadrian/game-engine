cfef1faeae4e3ecb3b7a3bc497fe6fbc49bad954
Nenu Adrian <adrian.nenu@gmail.com>
2024-06-02 13:16:05 -0400

    script compile inside asset window

--- src/core/editor/editor_manager.cpp
+++ src/core/editor/editor_manager.cpp
@@ -10,6 +10,29 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <cstdio>
+#include <memory>
+#include <stdexcept>
+#include <array>
+
+namespace
+{
+  std::string exec(const char *cmd)
+  {
+    std::array<char, 128> buffer;
+    std::string result;
+    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+    if (!pipe)
+    {
+      throw std::runtime_error("popen() failed!");
+    }
+    while (fgets(buffer.data(), static_cast<int>(buffer.size()), pipe.get()) != nullptr)
+    {
+      result += buffer.data();
+    }
+    return result;
+  }
+}
 
 namespace Hades
 {
@@ -360,6 +383,21 @@ namespace Hades
                              { return e == selectedAsset; }),
               project->assets.end());
           selectedAsset = nullptr;
+        }
+
+        if (selectedAsset->type == AssetType::SCRIPT)
+        {
+          if (ImGui::Button("Compile Script"))
+          {
+            try
+            {
+              engine->info(exec("g++ -dynamiclib -o libdynamic.dylib dynamic.cpp"));
+            }
+            catch (std::exception &e)
+            {
+              engine->error(e.what());
+            }
+          }
         }
 
         ImGui::End();
--- src/core/engine.h
+++ src/core/engine.h
@@ -5,29 +5,33 @@
 #include <string>
 #include <vector>
 
-namespace Hades {
-class Log {
-public:
-  std::string msg;
+namespace Hades
+{
+  class Log
+  {
+  public:
+    std::string msg;
 
-  Log(std::string msg) : msg(msg){};
-};
-
-class Engine {
-
-public:
-  Events events;
-  std::vector<Log> logs;
-
-  Engine(){};
-  void RunEditor();
-  void RunGame();
-  static long int newEngineId() {
-    return std::chrono::system_clock::now().time_since_epoch().count();
+    Log(std::string msg_) : msg(msg_){};
   };
 
-  void info(std::string msg) { logs.push_back(Log(msg)); }
+  class Engine
+  {
 
-  void error(std::string msg) { logs.push_back(Log(msg)); }
-};
+  public:
+    Events events;
+    std::vector<Log> logs;
+
+    Engine(){};
+    void RunEditor();
+    void RunGame();
+    static long int newEngineId()
+    {
+      return std::chrono::system_clock::now().time_since_epoch().count();
+    };
+
+    void info(std::string msg) { logs.push_back(Log(msg)); }
+
+    void error(std::string msg) { logs.push_back(Log(msg)); }
+  };
 } // namespace Hades
