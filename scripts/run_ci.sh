ba8a3ba422b6968d2e4b993a8c4f4881ae7b603f
Adrian (Mircea) Nenu <adrian.nenu@gmail.com>
2024-06-21 17:13:33 -0400

    Update run_ci.sh

--- scripts/run_ci.sh
+++ scripts/run_ci.sh
@@ -20,6 +20,26 @@ apt-get --assume-yes install libgoogle-glog-dev
 
 apt-get --assume-yes install libfmt-dev `
 
+apt install -y liblz4-dev
+
+apt install -y libzstd-dev
+
+apt-get install libbz2-dev
+
+apt install libsnappy-dev
+
+apt-get install -y bzip2
+
+apt-get install -y libiberty-dev
+
+apt-get install -y libaio-dev
+
+apt-get install -y liburing-dev
+
+apt-get install -y libdwarf-dev
+
+apt-get install -y libsodium-dev
+
 git clone https://github.com/libsdl-org/SDL.git -b SDL2
 
 cd SDL &&  mkdir build && cd build && ../configure && make && make install
